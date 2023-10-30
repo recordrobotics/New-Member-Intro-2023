99e791184b8e56568e053d1421bc8ed04a5e7650
Mmohet <jerry20080829@hotmail.com>
2023-10-30 19:47:31 -0400

    Create a model class

--- src/main/java/org/recordrobotics/intro/subsystems/ExampleSubsystem.java
+++ src/main/java/org/recordrobotics/intro/subsystems/ExampleSubsystem.java
@@ -1,22 +1,39 @@
+
 // Copyright (c) FIRST and other WPILib contributors.
 // Open Source Software; you can modify and/or share it under the terms of
 // the WPILib BSD license file in the root directory of this project.
 
 package org.recordrobotics.intro.subsystems;
 
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonFX;
+// import com.revrobotics.CANSparkMax;
+// import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
+
+
 public class ExampleSubsystem extends SubsystemBase {
-  /** Creates a new ExampleSubsystem. */
-  public ExampleSubsystem() {}
+    /** Creates a new ExampleSubsystem. */
+
+  // private CANSparkMax motorName = new CANSparkMax(5, MotorType.kBrushless);
+
+  private WPI_TalonFX motorName = new WPI_TalonFX(500);
+
+
+  public ExampleSubsystem() {
+    
+  }
 
   @Override
   public void periodic() {
     // This method will be called once per scheduler run
+    motorName.set(0.2);
   }
 
   @Override
   public void simulationPeriodic() {
     // This method will be called once per scheduler run during simulation
+
   }
 }
