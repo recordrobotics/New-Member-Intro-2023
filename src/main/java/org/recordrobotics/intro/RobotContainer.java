629d797a58e479082fd13971c358ca3a19e8d6fb
Person-26 <95951869+Person-26@users.noreply.github.com>
2023-11-09 20:22:15 -0500

    Teleop fix

--- src/main/java/org/recordrobotics/intro/RobotContainer.java
+++ src/main/java/org/recordrobotics/intro/RobotContainer.java
@@ -14,9 +14,12 @@ import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
 
 /**
- * This class is where the bulk of the robot should be declared. Since Command-based is a
- * "declarative" paradigm, very little robot logic should actually be handled in the {@link Robot}
- * periodic methods (other than the scheduler calls). Instead, the structure of the robot (including
+ * This class is where the bulk of the robot should be declared. Since
+ * Command-based is a
+ * "declarative" paradigm, very little robot logic should actually be handled in
+ * the {@link Robot}
+ * periodic methods (other than the scheduler calls). Instead, the structure of
+ * the robot (including
  * subsystems, commands, and button mappings) should be declared here.
  */
 public class RobotContainer {
@@ -28,9 +31,11 @@ public class RobotContainer {
 	// The robot's subsystems and commands are defined here...
 	private final ExampleSubsystem m_exampleSubsystem = new ExampleSubsystem();
 
-	private final ExampleManual m_autoCommand = new ExampleManual(m_exampleSubsystem, m_singleControl);
+	private final ExampleManual m_teleopCommand = new ExampleManual(m_exampleSubsystem, m_singleControl);
 
-	/** The container for the robot. Contains subsystems, OI devices, and commands. */
+	/**
+	 * The container for the robot. Contains subsystems, OI devices, and commands.
+	 */
 	public RobotContainer() {
 		// Configure the button bindings
 		_controlInput = new SingleControl(RobotMap.Control.LEGACY_GAMEPAD);
@@ -38,24 +43,27 @@ public class RobotContainer {
 		System.out.println(_controlInput != null);
 	}
 
-	/** la dee da dee da
-	 * Use this method to define your button->command mappings. Buttons can be created by
+	/**
+	 * la dee da dee da
+	 * Use this method to define your button->command mappings. Buttons can be
+	 * created by
 	 * instantiating a {@link GenericHID} or one of its subclasses ({@link
-	 * edu.wpi.first.wpilibj.Joystick} or {@link XboxController}), and then passing it to a {@link
+	 * edu.wpi.first.wpilibj.Joystick} or {@link XboxController}), and then passing
+	 * it to a {@link
 	 * edu.wpi.first.wpilibj2.command.button.JoystickButton}.
 	 */
-	private void configureButtonBindings() {}
-
-	/**
-	 * Use this to pass the autonomous command to the main {@link Robot} class.
-	 *
-	 * @return the command to run in autonomous
-	 */
-	public Command getAutonomousCommand() {
-		// An ExampleCommand will run in autonomous
-		return m_autoCommand;
+	private void configureButtonBindings() {
 	}
-	
+
+	public Command getTeleopCommand() {
+		// An ExampleCommand will run in autonomous
+		return m_teleopCommand;
+	}
+
+	public void teleopInit() {
+		m_exampleSubsystem.setDefaultCommand(m_teleopCommand);
+	}
+
 	/**
 	 * Clear commands
 	 */
